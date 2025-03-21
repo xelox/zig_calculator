956a86c210f5d5c7e5de3fe6d9bacc7e1b0941e3
xelox <alexdoppiaj@gmail.com>
2025-03-21 22:09:44 +0200

    added documentation comments to Rc and renamed some methods

--- src/ref_counter.zig
+++ src/ref_counter.zig
@@ -16,41 +16,43 @@ pub fn Rc(comptime T: type) type {
         been_released: bool = false,
         data: if (is_slice) T else *T,
 
-        ///
-        pub fn init(alloc: Allocator, data: T) !@This() {
+        /// Initializes a new Rc by copying memory from source to a new region in memory.
+        pub fn init(alloc: Allocator, source: T) !@This() {
             const ref_counter_ptr = try alloc.create(usize);
             errdefer alloc.destroy(ref_counter_ptr);
             ref_counter_ptr.* = 1;
 
             if (is_slice) {
                 const child_type = std.meta.Child(T);
-                const data_slice = try alloc.alloc(child_type, data.len);
-                @memcpy(data_slice, data);
+                const data_slice = try alloc.alloc(child_type, source.len);
+                @memcpy(data_slice, source);
                 return .{ .ref_counter = ref_counter_ptr, .data = data_slice };
             } else {
                 const data_ptr = try alloc.create(T);
-                data_ptr.* = data;
+                data_ptr.* = source;
                 return .{ .ref_counter = ref_counter_ptr, .data = data_ptr };
             }
         }
 
-        pub fn manage(alloc: Allocator, data: if (is_slice) T else *T) !@This() {
+        /// Initializes a new Rc by taking ownership of the source memory. No copy will be made.
+        pub fn manage(alloc: Allocator, source: if (is_slice) T else *T) !@This() {
             const ref_counter_ptr = try alloc.create(usize);
             errdefer alloc.destroy(ref_counter_ptr);
             ref_counter_ptr.* = 1;
-            return .{ .ref_counter = ref_counter_ptr, .data = data };
+            return .{ .ref_counter = ref_counter_ptr, .data = source };
         }
 
-        pub fn aquire(self: *const @This()) @This() {
+        /// Increase the ref counter and get a new Rc.
+        pub fn clone(self: *const @This()) @This() {
             self.ref_counter.* += 1;
             return .{ .ref_counter = self.ref_counter, .data = self.data };
         }
 
+        /// Release an instance of Rc and free if all references have been released.
+        /// calling twice on the same instance does nothing in release builds, but it panics in debug builds.
         pub fn release(self: *const @This(), alloc: Allocator) void {
             std.debug.assert(!self.been_released);
-            if (self.been_released) {
-                return;
-            }
+            if (self.been_released) return;
             @constCast(self).been_released = true;
             self.ref_counter.* -= 1;
             if (self.ref_counter.* == 0) {
@@ -70,7 +72,7 @@ test "Rc f64" {
     const a = try Rc(f64).init(alloc, 420.69);
     defer a.release(alloc);
 
-    const b = a.aquire();
+    const b = a.clone();
     defer b.release(alloc);
 
     try std.testing.expectEqual(420.69, a.data.*);
@@ -82,7 +84,7 @@ test "Rc slice" {
     const a = try Rc([]u8).init(alloc, @constCast(&[_]u8{ 0, 1, 2, 3 }));
     defer a.release(alloc);
 
-    const b = a.aquire();
+    const b = a.clone();
     defer b.release(alloc);
 
     try std.testing.expectEqualSlices(u8, &[_]u8{ 0, 1, 2, 3 }, a.data);
@@ -100,7 +102,7 @@ test "Rc random slice" {
     const a = try Rc([]u8).init(alloc, buffer);
     defer a.release(alloc);
 
-    const b = a.aquire();
+    const b = a.clone();
     defer b.release(alloc);
 
     try std.testing.expect(buffer.ptr != a.data.ptr);
@@ -121,7 +123,7 @@ test "Rc manage random slice" {
     const a = try Rc([]u8).manage(alloc, buffer);
     defer a.release(alloc);
 
-    const b = a.aquire();
+    const b = a.clone();
     defer b.release(alloc);
 
     try std.testing.expect(buffer.ptr == a.data.ptr);
@@ -141,7 +143,7 @@ test "Rc maange u64" {
     const a = try Rc(u64).manage(alloc, my_ptr);
     defer a.release(alloc);
 
-    const b = a.aquire();
+    const b = a.clone();
     defer b.release(alloc);
 
     try std.testing.expectEqual(my_ptr, a.data);
